d0d6a1d593de2b913d7d8eb431226fa1e2ca41f9
zeeptobean <zeeptobean@gmail.com>
2025-10-04 16:44:36 +0700

    wip 04/10 #3

---
+++ content/various/Setup.h
@@ -0,0 +1,40 @@
+/**
+ * Description: setup
+ */
+#pragma once
+// c_cpp_properties.json
+// {
+//   "configurations": [{
+//       "name": "Win32",
+//       "includePath": ["${workspaceFolder}/**"],
+//       "defines": ["_DEBUG","UNICODE","_UNICODE","LOCAL_FREOPEN"],
+//       "cStandard": "gnu17",
+//       "cppStandard": "gnu++17",
+//       "compilerPath": "C:/msys64/ucrt64/bin/g++.exe",
+//       "intelliSenseMode": "windows-gcc-x64"
+//   }],
+//   "version": 4
+// }
+// launch.json
+// {
+//   "version": "0.2.0",
+//   "configurations": [
+//   {
+//     "name": "msys2-mingw gdb 11.3 launch",
+//     "type": "cppdbg",
+//     "request": "launch",
+//     "program": "${fileDirname}/${fileBasenameNoExtension}.exe",
+//     "args": [],
+//     "cwd": "${fileDirname}",
+//     "MIMode": "gdb",
+//     "miDebuggerPath": "C:\\msys64\\ucrt64\\bin\\gdb.exe",
+//     "setupCommands": [
+//       {
+//         "description": "Enable pretty-printing for gdb",
+//         "text": "-enable-pretty-printing",
+//         "ignoreFailures": false
+//       }
+//     ]
+//   }
+//   ]
+// }
